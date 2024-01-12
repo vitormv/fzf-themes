921ccbe25610a67e6be424f16f3f467d1c8bff25
Vitor Mello <vitor@vmello.com>
2024-01-12 01:40:48 +0100

    fix deploy on CI

--- cli/gh-pages-deploy.sh
+++ cli/gh-pages-deploy.sh
@@ -4,4 +4,4 @@ git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f dist
 git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages
