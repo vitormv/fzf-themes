8467e1905a68f540f9722636c8e9b7190e3dc525
Vitor Mello <vitor@vmello.com>
2024-01-12 01:58:09 +0100

    setup base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'fzf-theme-playground',
   plugins: [svelte()],
   resolve: {
     alias: {
