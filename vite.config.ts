24dd6076f0b910f76f308f55b03e7d9af01c1a5b
Vitor Mello <vitor@vmello.com>
2024-01-19 19:00:00 +0100

    rename repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'fzf-theme-playground',
+  base: 'fzf-themes',
   plugins: [svelte(), mdPlugin.plugin({ mode: [Mode.HTML] })],
   resolve: {
     alias: {
